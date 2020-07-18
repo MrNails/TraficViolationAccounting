cf4dcb415d306b473dd9f79ce4ae22eedd607ffa
Денис Попович <popov.den30@gmail.com>
2020-07-18 15:47:02 +0300

    Added prtotype of new window

---
+++ AccountingOfTraficViolation/ViewModels/ParticipantInfoVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Windows;
+using AccountingOfTraficViolation.Models;
+using AccountingOfTraficViolation.Services;
+
+namespace AccountingOfTraficViolation.ViewModels
+{
+    public class ParticipantInfoVM
+    {
+    }
+}
