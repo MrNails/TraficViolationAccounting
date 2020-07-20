2adf12ece73a56f7100602cb8b200a4fb3ba70bd
Денис Попович <popov.den30@gmail.com>
2020-07-20 20:33:20 +0300

    Added window logic

--- AccountingOfTraficViolation/Views/AddInfoWindows/AddParticipantInfoWindow.xaml.cs
+++ AccountingOfTraficViolation/Views/AddInfoWindows/AddParticipantInfoWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using AccountingOfTraficViolation.Models;
+using AccountingOfTraficViolation.Services;
+using AccountingOfTraficViolation.ViewModels;
 
 namespace AccountingOfTraficViolation.Views.AddInfoWindows
 {
@@ -19,9 +23,70 @@ namespace AccountingOfTraficViolation.Views.AddInfoWindows
     /// </summary>
     public partial class AddParticipantInfoWindow : Window
     {
-        public AddParticipantInfoWindow()
+        public ParticipantInfoVM ParticipantInfoVM { get; private set; }
+
+        public AddParticipantInfoWindow() : this(null)
+        { }
+        public AddParticipantInfoWindow(ObservableCollection<ParticipantsInformation> participantsInfo)
         {
+            ParticipantInfoVM = new ParticipantInfoVM(participantsInfo);
+
             InitializeComponent();
+
+            DataContext = ParticipantInfoVM;
+
+            ParticipantsListBox.ItemsSource = ParticipantInfoVM.ParticipantsInformations;
+            ParticipantInfoGroupBox.Header = "Учавствующий № 1";
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox)
+            {
+                ComboBox comboBox = (ComboBox)sender;
+                if (comboBox.SelectedItem is ComboBoxItem && comboBox.SelectedItem != null && ((ComboBoxItem)comboBox.SelectedItem).Tag != null)
+                {
+                    if (((ComboBoxItem)comboBox.SelectedItem).Tag.ToString() == "1")
+                    {
+                        ParticipantInfoVM.CurrentParticipantsInformation.Gender = true;
+                    }
+                    else
+                    {
+                        ParticipantInfoVM.CurrentParticipantsInformation.Gender = false;
+                    }
+                }
+            }
+        }
+
+        private void PDDViolationTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            string tempStr = textBox.Text;
+            int caretIndex = textBox.CaretIndex;
+            int oldLength = textBox.Text.Length;
+
+            tempStr = tempStr.GetStrWithoutSeparator(',').AddSeparator(',', 2);
+
+            textBox.Text = tempStr;
+
+            //set caret after string change
+            if (caretIndex + (tempStr.Length - oldLength) >= 0)
+            {
+                textBox.CaretIndex = caretIndex + (tempStr.Length - oldLength);
+            }
+        }
+
+        private void ParticipantsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ParticipantsListBox.SelectedIndex >= 0)
+            {
+                ParticipantInfoVM.CurrentIndex = ParticipantsListBox.SelectedIndex;
+
+                GenderComboBox.SelectedIndex = ParticipantInfoVM.CurrentParticipantsInformation.Gender ? 1 : 0;
+
+                ParticipantInfoGroupBox.Header = "Учавствующий № " + (ParticipantInfoVM.CurrentIndex + 1).ToString();
+            }
         }
     }
 }
