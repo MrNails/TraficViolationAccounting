318ad10e9b6741c43e9d8f2c332f97e1b801c003
Денис Попович <popov.den30@gmail.com>
2020-07-29 18:50:41 +0300

    Added logic and features

--- AccountingOfTraficViolation/Views/ShowCaseWindow.xaml.cs
+++ AccountingOfTraficViolation/Views/ShowCaseWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace AccountingOfTraficViolation.Views
         private CasesVM casesVM;
         private User user;
         private CancellationTokenSource cancellationTokenSource;
+
         public ShowCaseWindow(User user)
         {
             InitializeComponent();
@@ -47,8 +48,51 @@ namespace AccountingOfTraficViolation.Views
             {
                 Button button = (Button)sender;
                 button.Content = "Отменить";
+                ComboBoxItem statusItem = CaseStatusComboBox.SelectedItem as ComboBoxItem;
 
-                string login = string.Copy(FindLoginTextBox.Text);
+                string login = null;
+                string status = null;
+                DateTime exactDate = default(DateTime);
+                DateTime startDate = default(DateTime);
+                DateTime endDate = default(DateTime);
+
+                if (string.IsNullOrEmpty(FindLoginTextBox.Text) && AllDateRadioButton.IsChecked == true &&
+                    statusItem != null && statusItem.Tag.ToString() == "1")
+                {
+                    button.Content = "Найти";
+                    MessageBox.Show("Вы не можете выбрать все дела.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                login = string.Copy(FindLoginTextBox.Text);
+                status = GetStatusFromComboBox(statusItem);
+
+                if (ExactDateRadioButton.IsChecked == true && ExactDateDatePicker.SelectedDate.HasValue)
+                {
+                    exactDate = ExactDateDatePicker.SelectedDate.Value;
+                }
+                else if (RangeDateRadioButton.IsChecked == true && StartDateDatePicker.SelectedDate.HasValue &&
+                         EndDateDatePicker.SelectedDate.HasValue)
+                {
+                    if (StartDateDatePicker.SelectedDate.Value >= MainTable.MinimumDate)
+                    {
+                        startDate = StartDateDatePicker.SelectedDate.Value;
+                    }
+                    else
+                    {
+                        startDate = MainTable.MinimumDate;
+                    }
+
+                    if (EndDateDatePicker.SelectedDate.Value >= MainTable.MinimumDate)
+                    {
+                        endDate = EndDateDatePicker.SelectedDate.Value;
+                    }
+                    else
+                    {
+                        endDate = MainTable.MinimumDate;
+                    }
+                }
+
 
                 if (cancellationTokenSource != null)
                 {
@@ -67,7 +111,11 @@ namespace AccountingOfTraficViolation.Views
                 {
                     await casesVM.FindCaseAsync(contex =>
                     {
-                        return contex.Cases.Where(c => c.CreaterLogin == login || c.CreaterLogin.Contains(login));
+                        return contex.Cases.Where(c => (string.IsNullOrEmpty(login) || c.CreaterLogin == login || c.CreaterLogin.Contains(login)) &&
+                                                       (string.IsNullOrEmpty(status) || c.State == status || c.State.Contains(status)) &&
+                                                       (exactDate == default(DateTime) || exactDate < MainTable.MinimumDate || c.OpenAt == exactDate) &&
+                                                       (startDate == default(DateTime) || startDate < MainTable.MinimumDate || c.OpenAt >= startDate) &&
+                                                       (endDate == default(DateTime) || c.OpenAt <= endDate));
                     }, cancellationTokenSource.Token);
                 }
                 catch (Exception ex)
@@ -83,9 +131,41 @@ namespace AccountingOfTraficViolation.Views
             }
         }
 
+        private string GetStatusFromComboBox(ComboBoxItem comboBoxItem)
+        {
+            string status = null;
+
+            switch (comboBoxItem.Tag.ToString())
+            {
+                case "2":
+                    status = "PROCESSING";
+                    break;
+                case "3":
+                    status = "CLOSE";
+                    break;
+                default:
+                    break;
+            }
+
+            return status;
+        }
+
         private void CasesGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             MessageBox.Show("+");
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is DatePicker)
+            {
+                DatePicker datePicker = (DatePicker)sender;
+
+                if (datePicker.SelectedDate < MainTable.MinimumDate)
+                {
+                    datePicker.SelectedDate = MainTable.MinimumDate;
+                }
+            }
         }
     }
 }
