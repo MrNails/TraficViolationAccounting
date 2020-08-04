29429664b8dd505746f5553686b3290b7494c5af
Денис Попович <popov.den30@gmail.com>
2020-08-04 16:22:58 +0300

    Added admin window and logic

---
+++ AccountingOfTraficViolation/ViewModels/UserVM.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountingOfTraficViolation.Models;
+using AccountingOfTraficViolation.Services;
+
+namespace AccountingOfTraficViolation.ViewModels
+{
+    public class UserVM : IDisposable, INotifyPropertyChanged
+    {
+        private TVAContext TVAContext;
+        private User currentFindUser;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public UserVM()
+        {
+            TVAContext = new TVAContext();
+        }
+
+        public bool IsCurrentUserChanged
+        {
+            get { return CurrentUser != null && TVAContext.Entry(CurrentUser).State == System.Data.Entity.EntityState.Modified; }
+        } 
+
+        public User CurrentUser
+        {
+            get { return currentFindUser; }
+            private set
+            {
+                currentFindUser = value;
+                OnPropertyChanged("CurrentUser");
+            }
+        }
+
+        public bool CheckIfCurrenUserLoginExist()
+        {
+            var res = TVAContext.Users.
+                                     Where(u => u.Login == CurrentUser.Login).
+                                     AsEnumerable().
+                                     Where(u => u.Login == CurrentUser.Login).
+                                     ToArray().
+                                     FirstOrDefault();
+            return res != null;
+        }
+
+        public void ConfirmChange()
+        {
+            if (CurrentUser == null)
+            {
+                return;
+            }
+
+            User user = TVAContext.Users.FirstOrDefault(u => u.Id == CurrentUser.Id);
+
+            if (user == null)
+            {
+                TVAContext.Users.Add(CurrentUser);
+            }
+
+            TVAContext.SaveChanges();
+        }
+        public void DiscardChange()
+        {
+            TVAContext.CancelAllChanges();
+        }
+        public void DeleteCurrentUser()
+        {
+            if (CurrentUser == null)
+            {
+                return;
+            }
+
+            TVAContext.Users.Remove(CurrentUser);
+            CurrentUser = null;
+            TVAContext.SaveChanges();
+        }
+        public void AddNewUser(byte role = 1)
+        {
+            CurrentUser = new User();
+            CurrentUser.Role = role;
+        }
+        public void SetCurrentUser(string login)
+        {
+            if (string.IsNullOrEmpty(login))
+            {
+                throw new ArgumentException("Логин не может отсутствовать.", "login");
+            }
+
+            CurrentUser = TVAContext.Users.
+                                     Where(u => u.Login == login).
+                                     AsEnumerable().
+                                     Where(u => u.Login == login).
+                                     ToArray().
+                                     FirstOrDefault();
+        }
+
+        public void Dispose()
+        {
+            TVAContext.Dispose();
+        }
+
+        private void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ AccountingOfTraficViolation/Views/AdminWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using AccountingOfTraficViolation.Models;
+using AccountingOfTraficViolation.Services;
+using AccountingOfTraficViolation.ViewModels;
+
+namespace AccountingOfTraficViolation.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        private UserVM userVM;
+
+        public AdminWindow()
+        {
+            InitializeComponent();
+            userVM = new UserVM();
+
+            DataContext = userVM;
+            UserGroupBox.Header = "Пользователь отсутствует";
+
+            DiscardChangeButton.IsEnabled = false;
+            SaveChangeButton.IsEnabled = false;
+            DeleteUserButton.IsEnabled = false;
+        }
+
+        private void AcceptClick(object sender, RoutedEventArgs e)
+        {
+            if (UserGroupBox.CheckIfExistValidationError())
+            {
+                return;
+            }
+
+            if (userVM.CheckIfCurrenUserLoginExist())
+            {
+                MessageBox.Show("Аккаунт с таким логином существует.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            userVM.ConfirmChange();
+        }
+        private void RejectClick(object sender, RoutedEventArgs e)
+        {
+            userVM.DiscardChange();
+        }
+        private void FindUserClick(object sender, RoutedEventArgs e)
+        {
+            if (userVM.IsCurrentUserChanged &&
+                MessageBox.Show("Изменение не сохранены, продолжить?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+            {
+                userVM.DiscardChange();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(FindUserLoginTextBox.Text))
+            {
+                MessageBox.Show("Поле с логином не может быть пустым.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            userVM.SetCurrentUser(FindUserLoginTextBox.Text);
+
+            if (userVM.CurrentUser != null)
+            {
+                UserGroupBox.Header = "Найденный пользователь";
+                DiscardChangeButton.IsEnabled = true;
+                SaveChangeButton.IsEnabled = true;
+                DeleteUserButton.IsEnabled = true;
+            }
+            else
+            {
+                UserGroupBox.Header = "Пользователь отсутствует";
+                DiscardChangeButton.IsEnabled = false;
+                SaveChangeButton.IsEnabled = false;
+                DeleteUserButton.IsEnabled = false;
+            }
+        }
+        private void DeleteClick(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы уверены, что хотите удалить пользователя?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            userVM.DeleteCurrentUser();
+            UserGroupBox.Header = "Пользователь отсутствует";
+
+            DiscardChangeButton.IsEnabled = false;
+            SaveChangeButton.IsEnabled = false;
+            DeleteUserButton.IsEnabled = false;
+        }
+        private void CreateNewUserClick(object sender, RoutedEventArgs e)
+        {
+            userVM.AddNewUser();
+
+            UserGroupBox.Header = "Новый пользователь";
+
+            FindUserLoginTextBox.Text = "";
+
+            SaveChangeButton.IsEnabled = true;
+            DiscardChangeButton.IsEnabled = false;
+            DeleteUserButton.IsEnabled = false;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (userVM.IsCurrentUserChanged &&
+                MessageBox.Show("Изменение не сохранены, продолжить?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+            {
+                userVM.DiscardChange();
+                return;
+            }
+
+            userVM.Dispose();
+        }
+    }
+}
