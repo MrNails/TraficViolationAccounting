dd5eacb6d14323fc90b3673d1a022ac010734750
Денис Попович <popov.den30@gmail.com>
2020-10-18 22:12:58 +0300

    Changed modifier

--- AccountOfTraficViolationDB/AccountOfTraficViolationDB/Helpers/Attributes.cs
+++ AccountOfTraficViolationDB/AccountOfTraficViolationDB/Helpers/Attributes.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace AccountingOfTraficViolation.Services
 {
     [System.AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
-    sealed class NotAssignAttribute : Attribute
+    public sealed class NotAssignAttribute : Attribute
     {
     }
 }
