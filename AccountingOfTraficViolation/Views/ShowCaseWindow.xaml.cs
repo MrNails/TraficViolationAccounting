ff283e7c70297e46467d4015d468dd1585830b5b
Денис Попович <popov.den30@gmail.com>
2020-10-21 21:49:39 +0300

    Added save to document

--- AccountingOfTraficViolation/Views/ShowCaseWindow.xaml.cs
+++ AccountingOfTraficViolation/Views/ShowCaseWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using AccountingOfTraficViolation.Models;
 using AccountingOfTraficViolation.ViewModels;
 using AccountingOfTraficViolation.Services;
+using Microsoft.Win32;
 
 namespace AccountingOfTraficViolation.Views
 {
@@ -23,8 +24,11 @@ namespace AccountingOfTraficViolation.Views
     /// </summary>
     public partial class ShowCaseWindow : Window
     {
+        private bool isSaving;
+
         private CasesVM casesVM;
         private User user;
+
         private CancellationTokenSource cancellationTokenSource;
 
         public ShowCaseWindow(User user)
@@ -40,10 +44,16 @@ namespace AccountingOfTraficViolation.Views
             }
 
             DataContext = casesVM;
+
         }
 
         private async Task FindCase()
         {
+            bool loadResult = false;
+            CurrentAction.Content = "Загрузка данных из бд.";
+            ActionProgress.IsIndeterminate = true;
+            ActionProgress.Visibility = Visibility.Visible;
+
             if (cancellationTokenSource != null)
             {
                 cancellationTokenSource.Cancel();
@@ -61,51 +71,51 @@ namespace AccountingOfTraficViolation.Views
             DateTime startDate = default(DateTime);
             DateTime endDate = default(DateTime);
 
-            if (string.IsNullOrEmpty(FindLoginTextBox.Text) && AllDateRadioButton.IsChecked == true &&
-                statusItem != null && statusItem.Tag.ToString() == "1")
-            {
-                MessageBox.Show("Вы не можете выбрать все дела.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            login = string.Copy(FindLoginTextBox.Text);
-            status = GetStatusFromComboBox(statusItem);
-
-            if (ExactDateRadioButton.IsChecked == true && ExactDateDatePicker.SelectedDate.HasValue)
-            {
-                exactDate = ExactDateDatePicker.SelectedDate.Value;
-            }
-            else if (RangeDateRadioButton.IsChecked == true && StartDateDatePicker.SelectedDate.HasValue &&
-                     EndDateDatePicker.SelectedDate.HasValue)
-            {
-                if (StartDateDatePicker.SelectedDate.Value >= MainTable.MinimumDate)
-                {
-                    startDate = StartDateDatePicker.SelectedDate.Value;
-                }
-                else
-                {
-                    startDate = MainTable.MinimumDate;
-                }
-
-                if (EndDateDatePicker.SelectedDate.Value >= MainTable.MinimumDate)
-                {
-                    endDate = EndDateDatePicker.SelectedDate.Value;
-                }
-                else
-                {
-                    endDate = MainTable.MinimumDate;
-                }
-            }
-
             try
             {
-                await casesVM.FindCaseAsync(_case =>
+                if (string.IsNullOrEmpty(FindLoginTextBox.Text) && AllDateRadioButton.IsChecked == true &&
+                    statusItem != null && statusItem.Tag.ToString() == "1")
                 {
-                    return _case.Where (c => (string.IsNullOrEmpty(login) || c.CreaterLogin == login || c.CreaterLogin.Contains(login)) &&
-                           (string.IsNullOrEmpty(status) || c.State == status) &&
-                           (exactDate == default(DateTime) || exactDate < MainTable.MinimumDate || c.OpenAt == exactDate) &&
-                           (startDate == default(DateTime) || startDate < MainTable.MinimumDate || c.OpenAt >= startDate) &&
-                           (endDate == default(DateTime) || c.OpenAt <= endDate));
+                    throw new Exception("Вы не можете выбрать все дела.");
+                }
+
+                login = string.Copy(FindLoginTextBox.Text);
+                status = GetStatusFromComboBox(statusItem);
+
+                if (ExactDateRadioButton.IsChecked == true && ExactDateDatePicker.SelectedDate.HasValue)
+                {
+                    exactDate = ExactDateDatePicker.SelectedDate.Value;
+                }
+                else if (RangeDateRadioButton.IsChecked == true && StartDateDatePicker.SelectedDate.HasValue &&
+                         EndDateDatePicker.SelectedDate.HasValue)
+                {
+                    if (StartDateDatePicker.SelectedDate.Value >= MainTable.MinimumDate)
+                    {
+                        startDate = StartDateDatePicker.SelectedDate.Value;
+                    }
+                    else
+                    {
+                        startDate = MainTable.MinimumDate;
+                    }
+
+                    if (EndDateDatePicker.SelectedDate.Value >= MainTable.MinimumDate)
+                    {
+                        endDate = EndDateDatePicker.SelectedDate.Value;
+                    }
+                    else
+                    {
+                        endDate = MainTable.MinimumDate;
+                    }
+                }
+
+
+                loadResult = await casesVM.FindCaseAsync(_case =>
+                {
+                    return _case.Where(c => (string.IsNullOrEmpty(login) || c.CreaterLogin == login || c.CreaterLogin.Contains(login)) &&
+                          (string.IsNullOrEmpty(status) || c.State == status) &&
+                          (exactDate == default(DateTime) || exactDate < MainTable.MinimumDate || c.OpenAt == exactDate) &&
+                          (startDate == default(DateTime) || startDate < MainTable.MinimumDate || c.OpenAt >= startDate) &&
+                          (endDate == default(DateTime) || c.OpenAt <= endDate));
                 }, cancellationTokenSource.Token);
             }
             catch (OperationCanceledException ex)
@@ -123,16 +133,29 @@ namespace AccountingOfTraficViolation.Views
             finally
             {
                 cancellationTokenSource = null;
+                if (loadResult)
+                {
+                    CurrentAction.Content = "Данные из бд загружены.";
+                }
+                else
+                {
+                    CurrentAction.Content = "Не удалось загрузить данные из бд.";
+                }
+
+                ActionProgress.IsIndeterminate = false;
+                ActionProgress.Visibility = Visibility.Hidden;
             }
+
+
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if (casesVM.CaseChanged && 
+            if (casesVM.CaseChanged &&
                 MessageBox.Show("Вы уверенны, что хотите выйти?\nВы можете потерять все изменения.", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
             {
                 e.Cancel = true;
-                return;    
+                return;
             }
 
             casesVM.Dispose();
@@ -145,9 +168,11 @@ namespace AccountingOfTraficViolation.Views
                 Button button = (Button)sender;
                 button.Content = "Отменить";
 
+
                 FindConditionExpander.IsExpanded = false;
 
                 await FindCase();
+
 
                 button.Content = "Найти";
             }
@@ -223,5 +248,81 @@ namespace AccountingOfTraficViolation.Views
             }
         }
 
+        private async void SaveDocumentClick(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement frameworkElement = sender as FrameworkElement;
+
+            if (frameworkElement == null)
+            {
+                return;
+            }
+
+            if (isSaving)
+            {
+                MessageBox.Show("Операция не возможна, так как на " +
+                                "данный момент сохраняется другой файл.", 
+                                "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string tag = frameworkElement.Tag.ToString();
+            string path = null;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            DocumentSaveType documentSaveType;
+
+            switch (tag)
+            {
+                case "1":
+                    saveFileDialog.Filter = "Word Files | *.docx";
+                    documentSaveType = DocumentSaveType.DOCX;
+                    break;
+                case "2":
+                    saveFileDialog.Filter = "PDF Files | *.pdf";
+                    documentSaveType = DocumentSaveType.PDF;
+                    break;
+                default:
+                    saveFileDialog.Filter = "Word Files | *.docx";
+                    documentSaveType = DocumentSaveType.DOCX;
+                    break;
+            }
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                path = saveFileDialog.FileName;
+            }
+            else
+            {
+                return;
+            }
+
+            CurrentAction.Content = $"Сохранение файла {saveFileDialog.SafeFileName}.";
+            ActionProgress.Minimum = 0;
+            ActionProgress.Maximum = 100;
+            ActionProgress.Visibility = Visibility.Visible;
+            
+            Binding savingProgressBinding = new Binding()
+            {
+                Source = casesVM.SaveCaseToWordVM,
+                Path = new PropertyPath("SavingProgress"),
+                Mode = BindingMode.OneWay,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            };
+
+            ActionProgress.SetBinding(ProgressBar.ValueProperty, savingProgressBinding);
+
+            try
+            {
+                isSaving = true;
+                await casesVM.SaveToDocument(path, documentSaveType);
+            }
+            finally
+            {
+                isSaving = false;
+                CurrentAction.Content = "Ничего не происходит.";
+                ActionProgress.Value = 0;
+                ActionProgress.Visibility = Visibility.Hidden;
+            }
+        }
     }
 }
