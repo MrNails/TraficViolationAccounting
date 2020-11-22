3fa22ff22d70587b828ea5b3c19e0297cf7da7ea
Денис Попович <popov.den30@gmail.com>
2020-11-22 14:03:28 +0200

    Added exception handler

--- AccountingOfTraficViolation/App.xaml.cs
+++ AccountingOfTraficViolation/App.xaml.cs
@@ -9,6 +9,7 @@ namespace AccountingOfTraficViolation
     public partial class App : Application
     {
         private Mutex currentAppInstance;
+        private ILogger logger;
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
@@ -19,6 +20,23 @@ namespace AccountingOfTraficViolation
             {
                 this.Shutdown();
             }
+
+            
+
+            logger = new FileLogger("Errors.txt");
+
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Возникла ошибка, смотри подробности в файле Errors.txt в папке приложения.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            logger.ErrorMessage = $"Сообщение: {e.Exception.Message}\nВнутренняя ошибка: " +
+                                  $"{(e.Exception.InnerException != null ? e.Exception.InnerException.Message : "")}" +
+                                  $"\nStackTrace: {e.Exception.StackTrace}";
+            logger.Log();
+
+            e.Handled = true;
         }
     }
 }
